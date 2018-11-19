70549615c30f649837cdf50306f0033bd639dff4
fellowseb <wauquier.sebastien@gmail.com>
2018-11-19 09:23:33 +0100

    thinkmachine/less: update config

--- thinkmachine/less/.config/profile.d/less.sh
+++ thinkmachine/less/.config/profile.d/less.sh
@@ -1,2 +1,12 @@
 # XDG dir spec compliance
 export LESSHISTFILE="${XDG_CACHE_HOME:-$HOME/.cache}"/less/history
+export LESSKEY="${XDG_CONFIG_HOME:-$HOME/.config}"/less/conf
+
+# Formatting / coloring
+export LESS_TERMCAP_mb=$'\e[1;31m'
+export LESS_TERMCAP_md=$'\e[1;34m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[38;5;16;48;5;4;1m'
+export LESS_TERMCAP_se=$'\e[39;49m'
+export LESS_TERMCAP_us=$'\e[4;96m'
+export LESS_TERMCAP_ue=$'\e[0m'
