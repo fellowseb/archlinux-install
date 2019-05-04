f5ac0daa4dd76f017408afb76b6a88b1aa65f113
fellowseb <wauquier.sebastien@gmail.com>
2019-05-04 23:18:27 +0200

    thinkmachine/aws: add AWS_SDK_LOAD_CONFIG env var
    This makes sure the shared credentials file
    is read by the AWS JS lib

--- thinkmachine/aws/.config/profile.d/aws.sh
+++ thinkmachine/aws/.config/profile.d/aws.sh
@@ -1,3 +1,4 @@
 # XDG dir spec compliance
 export AWS_SHARED_CREDENTIALS_FILE="${XDG_CONFIG_HOME:-$HOME/.config}"/aws/credentials
 export AWS_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}"/aws/config
+export AWS_SDK_LOAD_CONFIG=true
